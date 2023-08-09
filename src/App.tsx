d0dae43ae24d2f807584c98c6377a66e5876ab88
박준형 <108733795+krapli441@users.noreply.github.com>
2023-08-09 11:12:49 +0900

    html에 직접 삽입, 지도 생성.

--- src/App.tsx
+++ src/App.tsx
@@ -21,23 +21,8 @@ const appStyle = {
 const apiKey = process.env.REACT_APP_MAPS_API;
 
 const App: React.FC = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&libraries=services,clusterer`;
-    script.async = true;
-    document.head.appendChild(script);
-
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, []);
-
   return (
     <Box className="App" style={appStyle}>
-      <Text>
-        <code>cold case</code>
-      </Text>
       <KakaoMap />
     </Box>
   );
--- src/map/map.tsx
+++ src/map/map.tsx
@@ -14,7 +14,7 @@ const KakaoMap: React.FC = () => {
   return (
     <Map
       center={{ lat: 33.5563, lng: 126.79581 }}
-      style={{ width: "100%", height: "360px" }}
+      style={{ width: "80vw", height: "80vh" }}
     >
       <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
         <div style={{ color: "#000" }}>Hello World!</div>
