5d2f1847541a9896d5275e80407908e905cb744b
박준형 <108733795+krapli441@users.noreply.github.com>
2023-08-10 11:11:37 +0900

    우선 제목 추가

--- src/case/murdercase.ts
+++ src/case/murdercase.ts
@@ -1,13 +1,318 @@
 const murderCaseData = [
   {
-    title: "강릉 노파 쪽지문 살인 사건",
-    date: "2005년 5월 13일",
-    latlng: { lat: 37.69849700000001, lng: 128.8850399 },
+    title: "광명 초등학생 유괴살인사건",
+    date: "2003년 3월 30일",
+    latlng: { lat: 37.435621800000035, lng: 126.87899069999997 },
   },
   {
+    title: "광주 대인동 살인사건",
+    date: "2008년 10월 20일",
+    latlng: { lat: 35.15400490000001, lng: 126.9152254 },
+  },
+  {
+    title: "광주 여대생 테이프 살인사건",
+    date: "",
+    latlng: { lat: 35.17626600000002, lng: 126.894097 },
+  },
+  {
+    title: "광주 주유소장 살인사건",
+    date: "2005년 5월 16일",
+    latlng: { lat : 35.16502419999999, lng : 126.7528769},
+  },
+  {
+    title: "김포 가정주부 살인사건",
+    date: "2006년 5월 8일",
+    latlng: {lat: 37.60620830000001 , lng: 126.72235489999998}
+  },
+  {
+    title: "남양주 아파트 밀실 살인사건",
+    date: "2020년 11월 17일",
+    latlng: {lat: 37.72141680000003 , lng: 127.20558539999999}
+  },  {
+    title: "대구 아내 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "대구 초등생 납치 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "대구 총포사 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "대전 갈마동 빌라 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "대전 갈마동 월평산 여중생 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "대전 송촌동 택시기사 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "대전 자양동 여교사 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "돈암동 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "돌산도 컨테이너실 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "동해 학습지 여교사 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "만봉천 예비간호사 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "모텔 정화조 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "목포 여대생 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "무안 저수지 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "봉천동 주택가 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "부산 대교동 여관 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "부산 배산 여대생 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "부산 부전동 모텔 여주인 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "부산 시약산 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "부산 온천동 커피숍 여주인 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "부산 청테이프 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "부여 노파 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "부천 여월동 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "서울 지하철역 연쇄 납치 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "서울 향수업체 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "서천 카센터 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "센트럴 시티 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "속초시 콘도 살인 암매장사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "수원 신대저수지 토막 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "수원역 노숙 소녀 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "순창 손목 없는 백골 사체 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "신정동 연쇄폭행살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "안산 리어카 토막 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "양구 전당포 노부부 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "영동 여고생 살인 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "영등포 노들길 살인 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "영주 택시기사 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "완도 아령 살인 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "울산 단란주점 살인 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "울산 부곡동 살인 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "울산 초등생 방화 살인 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "의정부 여중생 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "이혼 부부 보험금 사망 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "인제 광치령 토막 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "인천 계양구 놀이터 살인 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "인천 십정동 부부 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "청주 물탱크실 주부 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "청주 미용강사 살인 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "청주 비닐봉지 살인 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "춘천 후평동 택시기사 살인 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "포천 여중생 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "화성 여대생 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "흥해 토막 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "파주 택시기사 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "대구 어린이 황산 테러 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "대구 여대생 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "대구 청테이프 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },  {
+    title: "대구 팔공산 팔공CC 캐디 토막살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  },
+  {
+    title: "대천 영유아 연쇄유괴살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
+    title: "부산 괘법동 태양다방 종업원 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
+    title: "부산 미용사 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
+    title: "부산 어린이 연쇄살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
+    title: "사바이 단란주점 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
+    title: "서귀포 호프집 여주인 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
+    title: "대구 성서 초등학생 살인 암매장 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
+    title: "자양면 영천호 토막 살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
+    title: "제주 보육교사 피살사건",
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
+    title: "이형호 유괴 살인 사건",
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
+    title: "치과의사 모녀살인사건",
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
     title: "아산 갱티고개 살인사건",
-    date: "2002년 7월",
-    latlng: { lat: 36.74302710000001, lng: 126.97880940000002 },
+    date: "",
+    latlng: {lat: , lng: }
+  }, {
+    title: "",
+    date: "",
+    latlng: {lat: , lng: }
   },
 ];
 
