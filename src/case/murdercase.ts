a25515f54b4b819b4a954e13ea43bfb86a5cd577
박준형 <108733795+krapli441@users.noreply.github.com>
2023-08-10 11:44:59 +0900

    좌표 업데이트

--- src/case/murdercase.ts
+++ src/case/murdercase.ts
@@ -17,302 +17,367 @@ const murderCaseData = [
   {
     title: "광주 주유소장 살인사건",
     date: "2005년 5월 16일",
-    latlng: { lat : 35.16502419999999, lng : 126.7528769},
+    latlng: { lat: 35.16502419999999, lng: 126.7528769 },
   },
   {
     title: "김포 가정주부 살인사건",
     date: "2006년 5월 8일",
-    latlng: {lat: 37.60620830000001 , lng: 126.72235489999998}
+    latlng: { lat: 37.60620830000001, lng: 126.72235489999998 },
   },
   {
     title: "남양주 아파트 밀실 살인사건",
     date: "2020년 11월 17일",
-    latlng: {lat: 37.72141680000003 , lng: 127.20558539999999}
-  },  {
+    latlng: { lat: 37.72141680000003, lng: 127.20558539999999 },
+  },
+  {
     title: "대구 아내 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.84455199999999, lng: 128.5562938 },
+  },
+  {
     title: "대구 초등생 납치 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.714740000000006, lng: 128.5017392 },
+  },
+  {
     title: "대구 총포사 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.82928880000001, lng: 128.595772 },
+  },
+  {
     title: "대전 갈마동 빌라 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 36.347403400000005, lng: 127.36949589999998 },
+  },
+  {
     title: "대전 갈마동 월평산 여중생 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 36.33252480000002, lng: 127.3612166 },
+  },
+  {
     title: "대전 송촌동 택시기사 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 36.36269409999999, lng: 127.44199470000001 },
+  },
+  {
     title: "대전 자양동 여교사 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.581072600000006, lng: 129.08273730000002 },
+  },
+  {
     title: "돈암동 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.5977955, lng: 127.01828289999997 },
+  },
+  {
     title: "돌산도 컨테이너실 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 34.64399199999999, lng: 127.76154159999999 },
+  },
+  {
     title: "동해 학습지 여교사 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.581072600000006, lng: 129.08273730000002 },
+  },
+  {
     title: "만봉천 예비간호사 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 34.911684599999994, lng: 126.7723926 },
+  },
+  {
     title: "모텔 정화조 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.82943739999999, lng: 128.5655119 },
+  },
+  {
     title: "목포 여대생 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 34.80941209999999, lng: 126.41615789999999 },
+  },
+  {
     title: "무안 저수지 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 34.938426, lng: 126.44406189999998 },
+  },
+  {
     title: "봉천동 주택가 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.47796710000001, lng: 126.95348829999999 },
+  },
+  {
     title: "부산 대교동 여관 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.09092800000001, lng: 129.04145570000003 },
+  },
+  {
     title: "부산 배산 여대생 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.18, lng: 129.09638890000005 },
+  },
+  {
     title: "부산 부전동 모텔 여주인 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.15472400000003, lng: 129.0571687 },
+  },
+  {
     title: "부산 시약산 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.11854830000001, lng: 128.99962249999996 },
+  },
+  {
     title: "부산 온천동 커피숍 여주인 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.2138813, lng: 129.0681841 },
+  },
+  {
     title: "부산 청테이프 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.124697800000035, lng: 129.0091427 },
+  },
+  {
     title: "부여 노파 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 36.24634589999999, lng: 126.856897 },
+  },
+  {
     title: "부천 여월동 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.51273450000002, lng: 126.80272070000001 },
+  },
+  {
     title: "서울 지하철역 연쇄 납치 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.65452700000004, lng: 127.060551 },
+  },
+  {
     title: "서울 향수업체 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 34.50211450000002, lng: 126.51664980000002 },
+  },
+  {
     title: "서천 카센터 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 36.074603899999985, lng: 126.6968565 },
+  },
+  {
     title: "센트럴 시티 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.504914199999995, lng: 127.00491509999999 },
+  },
+  {
     title: "속초시 콘도 살인 암매장사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 38.17600999999999, lng: 128.5194615 },
+  },
+  {
     title: "수원 신대저수지 토막 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.28670960000002, lng: 127.07419030000003 },
+  },
+  {
     title: "수원역 노숙 소녀 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.26643980000002, lng: 126.99940770000002 },
+  },
+  {
     title: "순창 손목 없는 백골 사체 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.4073449, lng: 127.10706720000002 },
+  },
+  {
     title: "신정동 연쇄폭행살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.515179500000016, lng: 126.8557093 },
+  },
+  {
     title: "안산 리어카 토막 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.31318070000001, lng: 126.77518520000001 },
+  },
+  {
     title: "양구 전당포 노부부 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 38.17817600000003, lng: 128.001272 },
+  },
+  {
     title: "영동 여고생 살인 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 36.178556199999974, lng: 127.77885479999998 },
+  },
+  {
     title: "영등포 노들길 살인 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.53127630000001, lng: 126.9088084 },
+  },
+  {
     title: "영주 택시기사 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 36.870501700000005, lng: 128.59767209999998 },
+  },
+  {
     title: "완도 아령 살인 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 34.350454899999995, lng: 127.04363949999998 },
+  },
+  {
     title: "울산 단란주점 살인 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.55281979999999, lng: 129.3249634 },
+  },
+  {
     title: "울산 부곡동 살인 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.49798850000003, lng: 129.3347569 },
+  },
+  {
     title: "울산 초등생 방화 살인 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.51504010000001, lng: 129.32891400000003 },
+  },
+  {
     title: "의정부 여중생 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.736188400000025, lng: 127.06843559999999 },
+  },
+  {
     title: "이혼 부부 보험금 사망 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.63832750000002, lng: 127.0073721 },
+  },
+  {
     title: "인제 광치령 토막 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 38.1317758, lng: 128.1476562 },
+  },
+  {
     title: "인천 계양구 놀이터 살인 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.529784400000004, lng: 126.7297927 },
+  },
+  {
     title: "인천 십정동 부부 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.476954700000036, lng: 126.6999375 },
+  },
+  {
     title: "청주 물탱크실 주부 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 36.61779550000001, lng: 127.48068690000002 },
+  },
+  {
     title: "청주 미용강사 살인 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 36.65070429999999, lng: 127.48771649999998 },
+  },
+  {
     title: "청주 비닐봉지 살인 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 36.443650399999996, lng: 127.45837630000003 },
+  },
+  {
     title: "춘천 후평동 택시기사 살인 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.882643500000015, lng: 127.74858210000002 },
+  },
+  {
     title: "포천 여중생 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.831744799999996, lng: 127.13850530000002 },
+  },
+  {
     title: "화성 여대생 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.2130853, lng: 126.9740742 },
+  },
+  {
     title: "흥해 토막 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 36.12890500000004, lng: 129.374133 },
+  },
+  {
     title: "파주 택시기사 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 37.731638499999995, lng: 126.71923370000002 },
+  },
+  {
     title: "대구 어린이 황산 테러 사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.88166319999999, lng: 128.6452162 },
+  },
+  {
     title: "대구 여대생 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.856278399999965, lng: 128.48859900000002 },
+  },
+  {
     title: "대구 청테이프 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  },  {
+    latlng: { lat: 35.82576040000001, lng: 128.63567030000002 },
+  },
+  {
     title: "대구 팔공산 팔공CC 캐디 토막살인사건",
     date: "",
-    latlng: {lat: , lng: }
+    latlng: { lat: 35.98917429999999, lng: 128.7233735 },
   },
   {
     title: "대천 영유아 연쇄유괴살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
+    latlng: { lat: 36.33316289999999, lng: 126.61294409999998 },
+  },
+  {
     title: "부산 괘법동 태양다방 종업원 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
+    latlng: { lat: 35.16490040000003, lng: 128.98572200000004 },
+  },
+  {
     title: "부산 미용사 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
+    latlng: { lat: 35.2110866, lng: 128.96608239999998 },
+  },
+  {
     title: "부산 어린이 연쇄살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
+    latlng: { lat: 35.07613729999999, lng: 128.96970880000003 },
+  },
+  {
     title: "사바이 단란주점 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
+    latlng: { lat: 37.5235332, lng: 127.02306590000003 },
+  },
+  {
     title: "서귀포 호프집 여주인 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
+    latlng: { lat: 33.25412050000004, lng: 126.560076 },
+  },
+  {
     title: "대구 성서 초등학생 살인 암매장 사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
+    latlng: { lat: 35.86937889999997, lng: 128.5105366 },
+  },
+  {
     title: "자양면 영천호 토막 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
+    latlng: { lat: 36.07128109999999, lng: 129.02678769999997 },
+  },
+  {
     title: "제주 보육교사 피살사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
+    latlng: { lat: 33.4596401, lng: 126.33813789999998 },
+  },
+  {
     title: "이형호 유괴 살인 사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
+    latlng: { lat: 37.53101810000006, lng: 127.02811309999997 },
+  },
+  {
     title: "치과의사 모녀살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
+    latlng: { lat: 37.61915940000003, lng: 126.93556689999998 },
+  },
+  {
     title: "아산 갱티고개 살인사건",
     date: "",
-    latlng: {lat: , lng: }
-  }, {
-    title: "",
-    date: "",
-    latlng: {lat: , lng: }
+    latlng: { lat: 36.743027099999985, lng: 126.97880940000002 },
   },
 ];
 
