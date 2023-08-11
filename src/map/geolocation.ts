8d60a5c90819d1ff0f0cba9ace2ec3ae0fa45f09
박준형 <108733795+krapli441@users.noreply.github.com>
2023-08-11 17:10:33 +0900

    위치 정보를 가져오지 못해도 지도를 표시하도록 설정

--- src/map/geolocation.ts
+++ src/map/geolocation.ts
@@ -2,13 +2,13 @@ function getCurrentPosition(): Promise<GeolocationCoordinates> {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        resolve(position.coords);
-        reject("error");
         console.log(position.coords.latitude, position.coords.longitude);
         console.log("사용자 접속 위치를 불러옴 : ", position.coords);
+        resolve(position.coords); // 위치 정보를 성공적으로 가져올 경우
       },
       (error) => {
         console.log(error);
+        reject("error"); // 오류가 발생할 경우
       },
       {
         enableHighAccuracy: true,
