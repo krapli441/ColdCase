ff6099310b1bc2dd8ed08a0b6c2107d88e1c23ab
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-01 11:02:07 +0900

    geolcoation console.log 삭제

--- src/map/geolocation.ts
+++ src/map/geolocation.ts
@@ -2,8 +2,6 @@ function getCurrentPosition(): Promise<GeolocationCoordinates> {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position.coords.latitude, position.coords.longitude);
-        console.log("사용자 접속 위치를 불러옴 : ", position.coords);
         resolve(position.coords); // 위치 정보를 성공적으로 가져올 경우
       },
       (error) => {
